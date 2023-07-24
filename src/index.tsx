f6e74de3fceab4d3f47ad831ecba3712f159e33b
James Collier <james.collier@vib.be>
2023-07-24 11:41:05 +0200

    Set svg height and width properties.
    If these properties are not set, then under some conditions browsers
    will render the svg with zero height and width.

--- src/index.tsx
+++ src/index.tsx
@@ -451,6 +451,8 @@ export const Molecule: React.FC<MoleculeProps> = (props: MoleculeProps) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox={`0 0 ${width} ${height}`}
+      width={props.width || 100}
+      height={props.height || 100}
       style={{ pointerEvents: "none" }}
     >
       <g
